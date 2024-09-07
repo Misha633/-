b5ffa66814ceae4bbfe59faaf6f6a960b0dc1ecb
Misha633 <mbalabaev2011@gmail.com>
2024-09-07 17:55:29 +0700

    Add files via upload

---
+++ черепашка-куб.py.py
@@ -0,0 +1,27 @@
+import turtle
+
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.backward(100)
+turtle.fillcolor("red")
+turtle.begin_fill()
+turtle.right(270)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+
+
+turtle.end_fill()
+
+
+
+
+
+
+
+
+
---
+++ черепашка-робот.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.backward(100)
+turtle.right(270)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+
+
+
+
+
+
+turtle.end_fill()
