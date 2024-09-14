13f88aa1c7556a602461afa61d4a6804899b1eec
Misha633 <mbalabaev2011@gmail.com>
2024-09-14 18:01:31 +0700

    Add files via upload

---
+++ Урок2,1черепашка.py
@@ -0,0 +1,48 @@
+import turtle
+import time
+
+#ТЗ1
+print("Готов?")
+time.sleep(1)
+print(5)
+time.sleep(1)
+print(4)
+time.sleep(1)
+print(3)
+time.sleep(1)
+print(2)
+time.sleep(1)
+print(1)
+time.sleep(1)
+
+turtle.color()
+turtle.goto(0,100)
+turtle.fillcolor(1, 0, 0)
+turtle.begin_fill()
+turtle.circle(50)
+turtle.end_fill()
+
+turtle.color()
+turtle.goto(0, -50)
+turtle.fillcolor(1, 0, 0)
+turtle.begin_fill()
+turtle.circle(75)
+turtle.end_fill()
+
+
+
+turtle.color()
+turtle.goto(0,-250)
+turtle.fillcolor(1, 0, 0)
+turtle.begin_fill()
+turtle.circle(100)
+turtle.end_fill()
+
+
+
+
+
+
+
+
+
